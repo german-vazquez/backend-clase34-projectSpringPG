6b18767eb885d7d12463bff795003756015798f7
German Vazquez <germanalfredovazquez@gmail.com>
2022-11-29 06:00:28 -0300

    exceptions

--- src/main/java/com/example/backendclase34projectSpringPG/BackendClase34ProjectSpringPgApplication.java
+++ src/main/java/com/example/backendclase34projectSpringPG/BackendClase34ProjectSpringPgApplication.java
@@ -1,5 +1,6 @@
 package com.example.backendclase34projectSpringPG;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendClase34ProjectSpringPgApplication {
 
 	public static void main(String[] args) {
+
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(BackendClase34ProjectSpringPgApplication.class, args);
 	}
 
